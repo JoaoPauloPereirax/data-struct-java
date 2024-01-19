b916b5f4642251372ec3b63246fca68eda510850
João Paulo Pereira <joaopaulop.dev@gmail.com>
2024-01-18 23:08:19 -0300

    Atividade 1

---
+++ index01.java
@@ -0,0 +1,11 @@
+public class index01 {
+       public static void main(String[] args) throws Exception {
+              var x = 10;
+              var y = x;
+              y = y + 1;
+              
+              System.out.println("O valor da variavel x eh: "+x);
+              System.out.println("O valor da variavel y eh: "+y); 
+
+       }
+   }
